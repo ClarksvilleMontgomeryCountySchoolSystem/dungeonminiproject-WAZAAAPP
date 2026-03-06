2439f13291a880f08d51cbec667ef06c9d231335
Eli Leutert <Elijah.Leutert594@students.cmcss.net>
2026-03-06 08:42:29 -0600

    iuggvhh

--- task1.py
+++ task1.py
@@ -1,8 +1,53 @@
+
+
+good = r"""                              / |                         
+                                     \ ^ | / \ / \
+                               (\ / \ / ^ / \ /) / ^ )
+                                 \  \ / ^ / \ ^ /
+                                      ) ^ ^  \     (
+                                     (^ ^ ^ \ )
+                                \___ \ /___ _ /_____ _/
+                                  [________________]
+                                   |              |
+                                   |     / /\\     |
+                                   |    < <( )>>    |
+                                   |     \ \//     |
+                                    \___________ _/
+                                        |    |
+                                        |    |
+                                        |    |
+                                        |    |
+                                        |    |
+                                        |    |
+                                        |    |
+"""
+
+bad = r"""
+                  \___ \ / ___
+                     _ / _____
+                       _ /
+                [________________]
+                        | |
+                    | / / \\ |
+                  | < < ( ) >> |
+                  |      \ \ // |
+                 \____________ /
+                       | |
+                       | |
+                       | |
+                       | |
+                       | |
+                       | |
+                       | |
+"""
+
 torch_lit = True
 
 if torch_lit:
     outcome = "Flicker: the torch works lighting up the cold air around it."
+    print(good)
 else:
     outcome = "Doom: the torch didn't work."
+    print(bad)
 
 print(outcome)
--- task5.py
+++ task5.py
@@ -1,9 +1,42 @@
+
+
+
+
+good = r"""
+
+ ,--.     ,--.
+(  O )   (  O )
+ `--'  \  `--'
+        \   _
+  >-.   /   /| 
+     `-.__.'
+"""
+
+
+
+bad = r"""
+
+
+      _..._
+    ,'     `.
+  ,'         `.
+,'    _   ,-.  `.
+|    (_)  `-'   |
+|        >      |
+|     ,----.    |
+|    /,-""-.\   |
+`.  |/      "  ,'
+  `.         ,'
+    `._____,'
+"""
 escaped = True
 
 if escaped:
     outcome = "Legend: I escaped yes."
+    print(good)
 else:
     outcome = "Doom: im stuck here"
+    print(bad)
 
 
 print(outcome)
